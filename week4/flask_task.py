57c3533c2970cb22b4daf56cfb5400a6d4b745d8
ezchuang <129792088+ezchuang@users.noreply.github.com>
2023-07-27 01:30:22 +0800

    renew comment

--- week4/flask_task.py
+++ week4/flask_task.py
@@ -5,37 +5,41 @@ from flask import session
 from flask import redirect
 
 app = Flask(__name__, static_folder = "public", static_url_path = "/")
+# session 密鑰
 app.secret_key = "you can't see me"
 
 # index page
 @app.route("/", methods=["GET"])
 def index():
-    session["SIGNED-IN"] = False
     return render_template("index.html")
 
-# 登入判斷
+# 登入判斷，POST將使用者帳密隱藏
 @app.route("/signin", methods=["POST"])
 def signin():
     user_account = request.form["account"]
     user_password = request.form["password"]
+    # 帳密驗證
     if user_account == "test" and user_password == "test":
+        # 紀錄 驗證通過資訊
         session["SIGNED-IN"] = True
         return redirect("/member")
     else:
         error_msg = "帳號、或密碼輸入錯誤"
+        # 建立 GET 方法的 url 列 (參數傳遞)
         return redirect("/error?message=" + error_msg)
 
 # 登出
 @app.route("/signout", methods=["GET"])
 def signout():
-    # 換到首頁做，確保每次登入都是初始狀態
-    # session["SIGNED-IN"] = False
+    # 去除 驗證通過資訊
+    session["SIGNED-IN"] = False
     return redirect("/")
 
 
 # 登入成功
 @app.route("/member", methods=["GET"])
 def member():
+    # 驗證 是否有通過資訊
     if session["SIGNED-IN"] == False:
         return redirect("/")
     else:
@@ -45,12 +49,14 @@ def member():
 @app.route("/error", methods=["GET"])
 def error():
     msg = request.args.get("message", "帳號、或密碼輸入錯誤")
+    # 承襲自 "/signin"，參數傳遞
     return render_template("error.html", message = msg)
 
 # 計算器
 @app.route("/square")
 def square():
     target = int(request.args.get("calculate", "0"))
+    # 參數傳遞
     return render_template("/square.html", result = target * target)
 
 
